897be18760582b864800093188ab0f278720e99e
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-10-09 17:01:02 -0700

    Add files via upload

---
+++ 00-run-cutadapt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source activate qiime2-2018.8
+
+mkdir 00-trimmed
+mkdir 00-trimmed/logs
+
+for item in `ls raw/*.R1.fastq.gz` 
+
+	do
+
+	filestem=$(basename $item .R1.fastq.gz)
+	R1=raw/$filestem.R1.fastq.gz
+	R2=raw/$filestem.R2.fastq.gz
+
+        cutadapt --pair-filter=both --error-rate=0.2 --discard-untrimmed \
+	-g ^GTGYCAGCMGCCGCGGTAA -G ^CCGYCAATTYMTTTRAGTTT \
+	-o 00-trimmed/${filestem}.R1.trimmed.fastq \
+	-p 00-trimmed/${filestem}.R2.trimmed.fastq $R1 $R2 \
+	2>&1 | tee -a 00-trimmed/logs/${filestem}.cutadapt.stderrout
+
+done
+
+source deactivate
