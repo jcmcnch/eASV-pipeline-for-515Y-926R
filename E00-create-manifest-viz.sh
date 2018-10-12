92fc0ce32a3ec5996994ddcbac8c92595195ab4e
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-10-12 11:41:24 -0700

    Add files via upload

---
+++ E00-create-manifest-viz.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#shortcut for making manifests based on fasta file names
+#script assumes you have the same number of FWD and REV reads and that they're named in a meaninful way (i.e. samplename.1.fastq.gz)
+
+cutME=".trimmed.SILVA_132_EUK.cdhit95pc_1.fastq" #the bit you want to cut from the file names, leaving only the sample name
+
+for item in `ls 00-fastq/*1.fastq` ; do echo `basename $item $cutME` | sed 's/_/-/' ;done > names
+for item in `ls 00-fastq/*1.fastq` ; do echo `basename $item $cutME` | sed 's/_/-/' ;done >> names
+
+for item in `ls 00-fastq/*1.fastq` ; do printf \$PWD/$item'\n'; done > reads
+for item in `ls 00-fastq/*2.fastq` ; do printf \$PWD/$item'\n'; done >> reads
+
+for item in `ls 00-fastq/*1.fastq` ; do printf "forward\n" ; done > direction
+for item in `ls 00-fastq/*2.fastq` ; do printf "reverse\n" ; done >> direction
+
+printf "sample-id,absolute-filepath,direction\n" > manifest-viz.csv
+paste -d, names reads direction >> manifest-viz.csv
+
+rm names reads direction
