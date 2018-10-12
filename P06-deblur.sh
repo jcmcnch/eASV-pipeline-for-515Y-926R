ade22957e4619f7a2ad70707736eb3932147c184
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-10-11 17:08:50 -0700

    Add files via upload

---
+++ P06-deblur.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir logs/06-deblur
+source activate qiime2-2018.8
+qiime deblur denoise-16S \
+  --i-demultiplexed-seqs 04-QCd/filtered_sequences.qza \
+  --p-trim-length 364 \
+  --p-sample-stats \
+  --output-dir 06-deblurred \
+  --verbose 2>&1 | tee -a logs/06-deblur/deblur.txt
