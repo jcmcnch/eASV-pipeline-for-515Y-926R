c1e2a5df405ae7806451ebd04ea5438f4dcd0a75
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-10-13 09:42:26 -0700

    Add files via upload

---
+++ E10-export-deblur-results.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source activate qiime2-2018.8
+qiime tools export --input-path 09-deblurred/representative_sequences.qza --output-path 09-deblurred/
+qiime tools export --input-path 09-deblurred/stats.qza --output-path 09-deblurred/
+qiime tools export --input-path 09-deblurred/table.qza --output-path 09-deblurred/
+biom convert -i 09-deblurred/feature-table.biom -o 09-deblurred/feature-table.biom.tsv --to-tsv
