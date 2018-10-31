1c41a3352210d429fd3e919df5a6534889dae381
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-10-31 16:41:17 -0700

    Update P00-create-manifest.sh

--- P00-create-manifest.sh
+++ P00-create-manifest.sh
@@ -3,7 +3,11 @@
 #shortcut for making manifests based on fasta file names
 #script assumes you have the same number of FWD and REV reads and that they're named in a meaninful way (i.e. samplename.1.fastq.gz)
 
-cutME=".trimmed.SILVA_132_PROK.cdhit95pc_1.fastq" #the bit you want to cut from the file names, leaving only the sample name
+#remove empty files
+find ./00-fastq -size  0 -print0 | xargs -0 rm --
+
+#the bit you want to cut from the file names, leaving only the sample name
+cutME=".trimmed.SILVA_132_PROK.cdhit95pc_1.fastq" 
 
 for item in `ls 00-fastq/*1.fastq` ; do echo `basename $item $cutME` | sed 's/_/-/' ;done > names
 for item in `ls 00-fastq/*1.fastq` ; do echo `basename $item $cutME` | sed 's/_/-/' ;done >> names
