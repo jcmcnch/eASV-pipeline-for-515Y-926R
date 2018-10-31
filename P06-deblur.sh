efe6e27f3b524deab30f34c215690875f9c9cb1d
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-10-31 10:27:07 -0700

    Update P06-deblur.sh

--- P06-deblur.sh
+++ P06-deblur.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
 mkdir logs/06-deblur
+
 source activate qiime2-2018.8
+
+trimlength=$1
+
+if [[ ${#1} -eq 0 ]] ; then
+    echo 'Please enter a trim length for deblur (keep this consistent between different runs if you want to intercompare data). Example P06-deblur.sh 363'
+    exit 0
+fi
+
 qiime deblur denoise-16S \
   --i-demultiplexed-seqs 04-QCd/filtered_sequences.qza \
-  --p-trim-length $1 \
+  --p-trim-length $trimlength \
   --p-sample-stats \
   --output-dir 06-deblurred \
   --verbose 2>&1 | tee -a logs/06-deblur/deblur.txt
