6f0c3ca8e17419bd4e003ef240cab01d5449a33a
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-10-31 10:23:54 -0700

    Update P03a-DADA2.sh

--- P03a-DADA2.sh
+++ P03a-DADA2.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+
 source activate qiime2-2018.8
+
+trunclenf=$1
+trunclenr=$2
+
+if [[ ${#1} -eq 0 ]] ; then
+    echo 'Please enter a trim length for the left (R1) read as an integer separated with a space from the script name. e.g. P03a-DADA2.sh 210 <right trim length>'
+    exit 0
+fi
+
+if [[ ${#2} -eq 0 ]] ; then
+    echo 'Please enter a trim length for the right (R2) read as an integer separated with a space from the script name. e.g. P03a-DADA2.sh <left trim length> 180'
+    exit 0
+fi
 
 qiime dada2 denoise-paired \
   --i-demultiplexed-seqs 16s.qza \
   --p-trim-left-f 0 \
   --p-trim-left-r 0 \
-  --p-trunc-len-f 214 \
-  --p-trunc-len-r 187 \
+  --p-trunc-len-f $trunclenf \
+  --p-trunc-len-r $trunclenr \
   --output-dir 03a-DADA2d \
   --p-n-threads 10 \
   --verbose
