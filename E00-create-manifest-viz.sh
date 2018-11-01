62ceb0bd1e13e5aa99b9450cadb647bbe1cf79fd
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-11-01 15:16:35 -0700

    Update E00-create-manifest-viz.sh

--- E00-create-manifest-viz.sh
+++ E00-create-manifest-viz.sh
@@ -7,7 +7,7 @@
 find ./00-fastq -size  0 -print0 | xargs -0 rm --
 
 #the bit you want to cut from the file names, leaving only the sample name
-cutME=".trimmed.SILVA_132_EUK.cdhit95pc_1.fastq"
+cutME=".trimmed.SILVA_132_and_PR2_EUK.cdhit95pc_1.fastq"
 
 for item in `ls 00-fastq/*1.fastq` ; do echo `basename $item $cutME` | sed 's/\_/\-/g' ;done > names
 for item in `ls 00-fastq/*1.fastq` ; do echo `basename $item $cutME` | sed 's/\_/\-/g' ;done >> names
