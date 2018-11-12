6788082cb4926cec0fe5836321acea9d0107d377
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-11-12 13:52:33 -0800

    Update P04a-export-DADA2-results.sh

--- P04a-export-DADA2-results.sh
+++ P04a-export-DADA2-results.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source activate qiime2-2018.8
-qiime tools export --input-path 03a-DADA2d/representative_sequences.qza --output-path 03a-DADA2d/
-qiime tools export --input-path 03a-DADA2d/denoising_stats.qza --output-path 03a-DADA2d/
-qiime tools export --input-path 03a-DADA2d/table.qza --output-path 03a-DADA2d/
-biom convert -i 03a-DADA2d/feature-table.biom -o 03a-DADA2d/feature-table.biom.tsv --to-tsv
+qiime tools export --input-path 03-DADA2d/representative_sequences.qza --output-path 03-DADA2d/
+qiime tools export --input-path 03-DADA2d/denoising_stats.qza --output-path 03-DADA2d/
+qiime tools export --input-path 03-DADA2d/table.qza --output-path 03-DADA2d/
+biom convert -i 03-DADA2d/feature-table.biom -o 03-DADA2d/feature-table.biom.tsv --to-tsv
 source deactivate
