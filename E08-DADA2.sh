2bd28b5a77f93f975733ef1008a6bd9115753f90
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-11-12 15:46:09 -0800

    Update E08-DADA2.sh

--- E08-DADA2.sh
+++ E08-DADA2.sh
@@ -5,7 +5,7 @@ qiime dada2 denoise-single \
   --i-demultiplexed-seqs 18s-concat.qza \
   --p-trim-left 0 \
   --p-trunc-len 0 \
-  --output-dir 08a-DADA2d \
+  --output-dir 08-DADA2d \
   --p-n-threads 10 \
   --verbose
 
