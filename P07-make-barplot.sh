4b4c63b4da88610c6031b16dc561fe87ef19a8b2
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-11-15 19:06:18 -0800

    Update and rename P10-make-barplot.sh to P07-make-barplot.sh

--- P07-make-barplot.sh
+++ P07-make-barplot.sh
@@ -2,9 +2,9 @@
 source activate qiime2-2018.8
 
 qiime taxa barplot \
-  --i-table 06-deblurred/table.qza \
-  --i-taxonomy 07-classified/classification.qza \
+  --i-table 03-DADA2d/table.qza \
+  --i-taxonomy 06-classified/classification.qza \
   --m-metadata-file sample-metadata.tsv \
-  --output-dir 08-barplots
+  --output-dir 07-barplots
 
 source deactivate
