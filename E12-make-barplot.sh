9647d0265bf946de825e8dc264c437d888063f60
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-11-15 18:54:16 -0800

    Update and rename E13-make-barplot.sh to E12-make-barplot.sh

--- E12-make-barplot.sh
+++ E12-make-barplot.sh
@@ -2,9 +2,9 @@
 source activate qiime2-2018.8
 
 qiime taxa barplot \
-  --i-table 09-deblurred/table.qza \
-  --i-taxonomy 11-classified/classification.qza \
+  --i-table 08-DADA2d/table.qza \
+  --i-taxonomy 10-classified/classification.qza \
   --m-metadata-file sample-metadata.tsv \
-  --output-dir 13-barplots
+  --output-dir 12-barplots
 
 source deactivate
