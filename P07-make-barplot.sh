32da920a3e0b14afbdc143f4d2c20a2e6bd3e8ad
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-11-15 19:07:36 -0800

    Update P07-make-barplot.sh

--- P07-make-barplot.sh
+++ P07-make-barplot.sh
@@ -3,7 +3,7 @@ source activate qiime2-2018.8
 
 qiime taxa barplot \
   --i-table 03-DADA2d/table.qza \
-  --i-taxonomy 06-classified/classification.qza \
+  --i-taxonomy 05-classified/classification.qza \
   --m-metadata-file sample-metadata.tsv \
   --output-dir 07-barplots
 
