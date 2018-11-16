b8e1ff769c87fd0e4a46cc060898e529952cde8e
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-11-15 19:02:59 -0800

    Update and rename P08-classify-eASVs.sh to P05-classify-eASVs.sh

--- P05-classify-eASVs.sh
+++ P05-classify-eASVs.sh
@@ -3,7 +3,7 @@ source activate qiime2-2018.8
 
 qiime feature-classifier classify-sklearn \
   --i-classifier /home/db/SILVA_132/qiime_db/SILVA_132_97_tax_sliced_to_Parada_primers_classifier.qza \
-  --i-reads 06-deblurred/representative_sequences.qza \
-  --output-dir 07-classified
+  --i-reads 03-DADA2d/representative_sequences.qza \
+  --output-dir 05-classified
 
 source deactivate
