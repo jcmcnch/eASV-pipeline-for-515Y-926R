67574455dcdb5b46ba1de8a656abaa82e51a0215
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-11-15 18:46:46 -0800

    Update and rename E11-classify-seqs.sh to E10-classify-seqs.sh

--- E10-classify-seqs.sh
+++ E10-classify-seqs.sh
@@ -3,7 +3,7 @@ source activate qiime2-2018.8
 
 qiime feature-classifier classify-sklearn \
   --i-classifier /home/db/SILVA_132/qiime_db/SILVA_132_97_tax_sliced_to_Parada_primers_classifier.qza \
-  --i-reads 09-deblurred/representative_sequences.qza \
-  --output-dir 11-classified
+  --i-reads /representative_sequences.qza \
+  --output-dir 10-classified
 
 source deactivate
