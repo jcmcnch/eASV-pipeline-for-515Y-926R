cb7bc644450d13585b367d3b97dccddad33084c7
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-11-21 14:38:06 -0800

    Add files via upload

---
+++ T01-import-99-OTU-tax.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source activate qiime2-2018.8
+
+qiime tools import \
+  --type 'FeatureData[Taxonomy]' \
+  --source-format HeaderlessTSVTaxonomyFormat \
+  --input-path /home/db/SILVA_132/qiime_db/SILVA_132_QIIME_release/taxonomy/taxonomy_all/99/consensus_taxonomy_7_levels.txt \
+  --output-path /home/db/SILVA_132/qiime_db/SILVA_132_99_tax.qza
+
+source deactivate
