7f0ac9f7670e29a7e5a41175d0133eb82f4f79fc
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2019-01-18 15:27:38 -0800

    Update split-biom-table.sh

--- split-biom-table.sh
+++ split-biom-table.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+source activate qiime2-2018.8
 
+qiime taxa filter-table \
+        --i-table 03-DADA2/table.qza \
+        --i-taxonomy 05-classifications/classification.qza \
+        --p-include Epsilonbacteraeota \
+        --output-dir 12-Epsilonbacteraeota-subset
+
+source deactivate
