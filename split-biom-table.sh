e177e50bed6153a77ba8e2610041d50fda4bb66e
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2019-01-18 15:34:29 -0800

    Update split-biom-table.sh

--- split-biom-table.sh
+++ split-biom-table.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+
+table=$1
+taxon=$2
+
+if [[ ${#1} -eq 0 ]] ; then
+    echo 'Please enter a table to split.'
+    exit 0
+fi
+
+if [[ ${#2} -eq 0 ]] ; then
+    echo 'Please enter a taxonomic name to sort by. e.g. Chloroplast or Metazoa (Animalia). Make sure to put quotes around names with spaces.'
+    exit 0
+fi
+
 source activate qiime2-2018.8
 
 qiime taxa filter-table \
-        --i-table 03-DADA2/table.qza \
+        --i-table $table \
         --i-taxonomy 05-classifications/classification.qza \
-        --p-include Epsilonbacteraeota \
-        --output-dir 12-Epsilonbacteraeota-subset
+        --p-include $taxon \
+        --output-dir ${table}_${taxon}-subset
 
 source deactivate
