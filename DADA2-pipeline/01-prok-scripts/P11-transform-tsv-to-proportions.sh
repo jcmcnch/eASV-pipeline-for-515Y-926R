45d67282587f6a9d5dcef76dcfc1d00ddabc7afb
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2019-03-05 17:08:46 -0800

    Added in subsetting and PhytoRef classification for 16S sequences

--- DADA2-pipeline/01-prok-scripts/P11-transform-tsv-to-proportions.sh
+++ DADA2-pipeline/01-prok-scripts/P11-transform-tsv-to-proportions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-for item in `ls exports/*.with-tax.tsv`
-	do
-	outdir=`echo $item | cut -d\/ -f1`
+for item in `ls 10-exports/*.with-tax.tsv`; do
+	
+	outdir=`dirname $item`
 	filestem=`basename $item .tsv`
+
 	transform-ESV-tsv-to-proportions.py --input $item --output $outdir/$filestem.proportions.tsv --minimum_abundance_filtered_output $outdir/$filestem.proportions-minabund-0.01.tsv --minimum_abundance_threshold 0.01 --cumulative_abundance_filtered_output $outdir/$filestem.proportions-cumabund-0.99.tsv --cumulative_abundance_threshold 0.99
+
 done
