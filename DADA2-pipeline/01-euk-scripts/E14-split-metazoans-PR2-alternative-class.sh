a22407cbac293d617215c3fe030225508be57408
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2019-03-12 13:22:48 -0700

    Added metazoan-only tables

--- DADA2-pipeline/01-euk-scripts/E14-split-metazoans-PR2-alternative-class.sh
+++ DADA2-pipeline/01-euk-scripts/E14-split-metazoans-PR2-alternative-class.sh
@@ -44,4 +44,20 @@ qiime taxa filter-table \
     --p-exclude "division_Metazoa" \
     --o-filtered-table 14-subsetting/split-tables/exclude_division_Metazoa_PR2_filtered_table.qza
 
+#Create metazoan-only table based on SILVA132
+
+qiime taxa filter-table \
+    --i-table 08-DADA2d/table.qza \
+    --i-taxonomy 10-classified/classification.qza \
+    --p-include "D_3__Metazoa (Animalia)" \
+    --o-filtered-table 14-subsetting/split-tables/include_D_3__Metazoa_Animalia_SILVA132_filtered_table.qza
+
+#Create metazoan-only table based on PR2
+
+qiime taxa filter-table \
+    --i-table 08-DADA2d/table.qza \
+    --i-taxonomy 14-subsetting/reclassified-PR2/fixed/taxonomy-without-spaces.qza \
+    --p-include "division_Metazoa" \
+    --o-filtered-table 14-subsetting/split-tables/include_division_Metazoa_PR2_filtered_table.qza
+
 source deactivate
