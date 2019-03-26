553a0e0bbee73c09212b64ada4b17c451d3a76c1
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2019-03-26 15:40:59 -0700

    Update P09-split-mito-chloro-PhytoRef-reclassify.sh

--- DADA2-pipeline/01-prok-scripts/P09-split-mito-chloro-PhytoRef-reclassify.sh
+++ DADA2-pipeline/01-prok-scripts/P09-split-mito-chloro-PhytoRef-reclassify.sh
@@ -80,7 +80,7 @@ qiime taxa filter-table \
   --i-table 03-DADA2d/table.qza \
   --i-taxonomy 05-classified/classification.qza \
   --p-exclude "D_4__Mitochondria,D_1__Cyanobacteria" \
-  --o-filtered-table 09-subsetting/split-tables/exclude_D_1__Cyanobacteria_exclude_D_4__Mitochondria_filtered_table.qza
+  --o-filtered-table 09-subsetting/split-tables/exclude_D_1__Cyanobacteria_exclude_D_4__Mitochondria_NOTE_excludes_chloroplasts_filtered_table.qza
 
 #Create Archaea-only table
 qiime taxa filter-table \
