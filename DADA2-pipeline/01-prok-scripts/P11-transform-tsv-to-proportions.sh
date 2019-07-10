bf19eafcc91bf025a2d2caec2ce1864e9bcf4f49
MicrobialMelody <50156185+MicrobialMelody@users.noreply.github.com>
2019-07-10 11:16:14 -0700

    Revert "Minor changes to P17 with renaming the default taxonomy column"
    This reverts commit 4198665373a44ec8e984c9c33543bf86346e4ce7.

--- DADA2-pipeline/01-prok-scripts/P11-transform-tsv-to-proportions.sh
+++ DADA2-pipeline/01-prok-scripts/P11-transform-tsv-to-proportions.sh
@@ -5,6 +5,6 @@ for item in `ls 10-exports/*.with-tax.tsv`; do
 	outdir=`dirname $item`
 	filestem=`basename $item .tsv`
 
-	/home/melody/Run055/March2019-eASV-515Y-926R/eASV-pipeline-for-515Y-926R/DADA2-pipeline/02-utility-scripts/transform-ESV-tsv-to-proportions.py --input $item --output $outdir/$filestem.proportions.tsv --minimum_abundance_filtered_output $outdir/$filestem.proportions-minabund-0.01.tsv --minimum_abundance_threshold 0.01 --cumulative_abundance_filtered_output $outdir/$filestem.proportions-cumabund-0.99.tsv --cumulative_abundance_threshold 0.99
+	transform-ESV-tsv-to-proportions.py --input $item --output $outdir/$filestem.proportions.tsv --minimum_abundance_filtered_output $outdir/$filestem.proportions-minabund-0.01.tsv --minimum_abundance_threshold 0.01 --cumulative_abundance_filtered_output $outdir/$filestem.proportions-cumabund-0.99.tsv --cumulative_abundance_threshold 0.99
 
 done
