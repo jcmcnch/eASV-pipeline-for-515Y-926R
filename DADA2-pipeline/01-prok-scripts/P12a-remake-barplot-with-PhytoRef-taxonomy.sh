90b3aab9293e50050e4da737003c14059acf334b
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2021-03-23 17:30:06 -0700

    Update P12a-remake-barplot-with-PhytoRef-taxonomy.sh

--- DADA2-pipeline/01-prok-scripts/P12a-remake-barplot-with-PhytoRef-taxonomy.sh
+++ DADA2-pipeline/01-prok-scripts/P12a-remake-barplot-with-PhytoRef-taxonomy.sh
@@ -5,6 +5,6 @@ qiime taxa barplot \
   --i-table 03-DADA2d/table.qza \
   --i-taxonomy 09-subsetting/tax-merged/chloroplasts-PhytoRef-reclassified-merged-classification.qza \
   --m-metadata-file sample-metadata.tsv \
-  --output-dir 07-barplots
+  --output-dir 07-barplots-PhytoRef-chloroplast-tax
 
 conda deactivate
