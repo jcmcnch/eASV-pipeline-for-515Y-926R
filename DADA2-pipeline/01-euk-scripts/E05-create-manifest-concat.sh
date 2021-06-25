d9917d21490a19d3edc711bd3aa302395a583bc4
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2021-06-25 14:40:01 -0700

    fix manifest script

--- DADA2-pipeline/01-euk-scripts/E05-create-manifest-concat.sh
+++ DADA2-pipeline/01-euk-scripts/E05-create-manifest-concat.sh
@@ -5,9 +5,9 @@
 
 cutME=".trimmed.SILVA_132_and_PR2_EUK.cdhit95pc_concatenated.fastq" #the bit you want to cut from the file names, leaving only the sample name
 
-for item in `ls 04-concatenated/*concatenated.fastq` ; do echo `basename $item $cutME` | sed 's/\_/\-/g' ;done > names
+for item in `ls 04-concatenated/*concatenated.fastq` ; do echo `basename $item $cutME | sed 's/\_/\-/g'` ; done > names
 
-for item in `ls 04-concatenated/*concatenated.fastq` ; do printf \$PWD/$item'\n'; done > reads
+for item in `ls 04-concatenated/*concatenated.fastq` ; do printf "$PWD/${item}\n"; done > reads
 
 for item in `ls 04-concatenated/*concatenated.fastq` ; do printf "forward\n" ; done > direction
 
