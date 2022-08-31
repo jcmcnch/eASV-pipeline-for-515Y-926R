cfc1d8b67729419ef073322be5a40e465722a7d5
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-08-31 11:37:56 -0700

    Update repo

--- qiime2-2022.8-DADA2-SILVA138.1-PR2_4.14.0/setup-scripts/02-download-qiime2-classifiers-qiime2-2022.8.sh
+++ qiime2-2022.8-DADA2-SILVA138.1-PR2_4.14.0/setup-scripts/02-download-qiime2-classifiers-qiime2-2022.8.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -i
 
-mkdir classification-db ; cd classification-db
+currdir=$PWD
+
+mkdir /home/$USER/databases/qiime2-classification-db ; cd /home/$USER/databases/qiime2-classification-db
 
 #download database files from OSF, raw artifacts, and scripts
 for item in w32ac nvgru ; do curl -O -J -L https://osf.io/$item/download ; done
 
-cd ..
+cd $currdir
 
-echo "See https://osf.io/z8arq/ for more info, including template scripts which you could modify to generate classifiers for a different qiime2 version."
+echo ""
+echo "Your classifiers are stored at /home/$USER/databases/qiime2-classification-db/"
+echo "Make sure the path to these classifiers is correctly specified in your config file."
+echo "See https://osf.io/z8arq/ and https://github.com/jcmcnch/515F-Y_926R_database_construction for more info, including template scripts which you could modify to generate classifiers for a different qiime2 version."
