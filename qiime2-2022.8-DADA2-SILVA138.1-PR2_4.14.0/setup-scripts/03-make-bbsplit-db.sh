e7969e6bc3b41e59955d3e46a2d83bf05cd9eb01
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-08-31 12:42:00 -0700

    Update repo

--- qiime2-2022.8-DADA2-SILVA138.1-PR2_4.14.0/setup-scripts/03-make-bbsplit-db.sh
+++ qiime2-2022.8-DADA2-SILVA138.1-PR2_4.14.0/setup-scripts/03-make-bbsplit-db.sh
@@ -16,5 +16,5 @@ chmod u+x make-dbs-bbsplit.sh ; ./make-dbs-bbsplit.sh
 cd $currdir
 
 echo ""
-echo "Your bbsplit database is stored at /home/$USER/databases/bbsplit-db/"
+echo "Your bbsplit database is stored at /home/$USER/databases/bbsplit-db/EUK-PROK-bbsplit-db/"
 echo "Make sure the path to this splitting database is correctly specified in your config file."
