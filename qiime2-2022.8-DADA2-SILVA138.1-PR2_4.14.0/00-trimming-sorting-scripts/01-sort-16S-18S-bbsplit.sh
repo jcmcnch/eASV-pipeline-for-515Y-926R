82e824c287bc308a3830abef5039c988fe56a7d5
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-08-31 12:58:59 -0700

    Update repo

--- qiime2-2022.8-DADA2-SILVA138.1-PR2_4.14.0/00-trimming-sorting-scripts/01-sort-16S-18S-bbsplit.sh
+++ qiime2-2022.8-DADA2-SILVA138.1-PR2_4.14.0/00-trimming-sorting-scripts/01-sort-16S-18S-bbsplit.sh
@@ -18,7 +18,7 @@ for item in `ls 01-trimmed/*.R1.trimmed.fastq`
 	R2in=01-trimmed/$filestem.R2.trimmed.fastq
 
 	bbsplit.sh usequality=f qtrim=f minratio=0.30 minid=0.30 pairedonly=f threads=20 -Xmx100g \
-	path=/home/db/bbsplit-db/EUK-PROK-bbsplit-db/ \
+	path=$bbsplitdb \
 	in=$R1in in2=$R2in basename=$filestem.trimmed.%_#.fastq \
 	2>&1 | tee -a logs/02-bbsplit/$filestem.bbsplit_log
 
