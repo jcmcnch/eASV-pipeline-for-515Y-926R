06bb0d2daf15951c6fe89d51b3e567146fb19c39
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-08-31 17:36:28 -0700

    Update repo

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/setup-scripts/00-install-qiime2-2022.8.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/setup-scripts/00-install-qiime2-2022.8.sh
@@ -7,10 +7,10 @@ conda update conda
 conda install -c conda-forge mamba
 
 #download yaml config file from qiime2 peeps
-wget https://data.qiime2.org/distro/core/qiime2-2022.8-py38-linux-conda.yml
+wget https://data.qiime2.org/distro/core/qiime2-2022.2-py38-linux-conda.yml
 
 #create environment using mamba
-mamba env create -n qiime2-2022.8 --file qiime2-2022.8-py38-linux-conda.yml
+mamba env create -n qiime2-2022.2 --file qiime2-2022.2-py38-linux-conda.yml
 
 # OPTIONAL CLEANUP
-rm qiime2-2022.8-py38-linux-conda.yml
+rm qiime2-2022.2-py38-linux-conda.yml
