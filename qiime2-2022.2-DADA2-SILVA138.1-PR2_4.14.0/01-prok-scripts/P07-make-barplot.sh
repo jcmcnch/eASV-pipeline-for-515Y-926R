8f86c85757ac39c95882792496d0c844d1f1f6ad
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-09-02 15:46:39 -0700

    Update repo

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-prok-scripts/P07-make-barplot.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-prok-scripts/P07-make-barplot.sh
@@ -2,10 +2,14 @@
 source ../515FY-926R.cfg
 conda activate $qiime2version
 
+timestamp=`date +"%y%m%d-%H%M"`
+
 qiime taxa barplot \
   --i-table 03-DADA2d/table.qza \
   --i-taxonomy 05-classified/classification.qza \
   --m-metadata-file sample-metadata.tsv \
   --output-dir 07-barplots
+
+mv 07-barplots/visualization.qzv 07-barplots/$timestamp.$studyName.barplot.qzv
 
 conda deactivate
