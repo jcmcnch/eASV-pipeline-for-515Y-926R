8779e1b68012d67dea42b5a48e739b521602ab9e
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-09-02 15:57:22 -0700

    automatically update repo

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-prok-scripts/P04-export-DADA2-results.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-prok-scripts/P04-export-DADA2-results.sh
@@ -7,4 +7,10 @@ qiime tools export --input-path 03-DADA2d/denoising_stats.qza --output-path 03-D
 qiime tools export --input-path 03-DADA2d/table.qza --output-path 03-DADA2d/
 biom convert -i 03-DADA2d/feature-table.biom -o 03-DADA2d/feature-table.biom.tsv --to-tsv
 
+for item in 03-DADA2d/*fasta 03-DADA2d/*tsv ; do
+
+	mv $item 03-DADA2d/$timestamp.$studyName.`basename $item`
+
+done
+
 conda deactivate
