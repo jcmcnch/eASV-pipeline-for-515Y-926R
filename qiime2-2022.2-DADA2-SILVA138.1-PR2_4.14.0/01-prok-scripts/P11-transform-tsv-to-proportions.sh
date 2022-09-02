9eae2224d4cbf1e365d3e1ce7a4f64ced7bd7819
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-09-02 16:14:50 -0700

    automatically update repo

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-prok-scripts/P11-transform-tsv-to-proportions.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-prok-scripts/P11-transform-tsv-to-proportions.sh
@@ -2,11 +2,22 @@
 source ../515FY-926R.cfg
 conda activate $qiime2version
 
+timestamp=`date +"%y%m%d-%H%M"`
+
 for item in `ls 10-exports/*.with-tax.tsv`; do
 	
 	outdir=`dirname $item`
 	filestem=`basename $item .tsv`
 
-	./scripts/transform-ESV-tsv-to-proportions.py --input $item --output $outdir/$filestem.proportions.tsv --minimum_abundance_filtered_output $outdir/$filestem.proportions-minabund-0.01.tsv --minimum_abundance_threshold 0.01 --cumulative_abundance_filtered_output $outdir/$filestem.proportions-cumabund-0.99.tsv --cumulative_abundance_threshold 0.99
+	./scripts/transform-ESV-tsv-to-proportions.py --input $item \
+	--output $outdir/$filestem.proportions.tsv \
+	--minimum_abundance_filtered_output $outdir/$filestem.proportions-minabund-0.01.tsv --minimum_abundance_threshold 0.01 \
+	--cumulative_abundance_filtered_output $outdir/$filestem.proportions-cumabund-0.99.tsv --cumulative_abundance_threshold 0.99
+
+done
+
+for item in 10-exports/*proportions* ; do
+
+        mv $item 10-exports/$timestamp.$studyName.`basename $item`
 
 done
