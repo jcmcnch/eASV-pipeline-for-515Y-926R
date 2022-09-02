685ea92a0dd0694a4fd791453b005aea7b5264a2
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-09-02 16:49:05 -0700

    automatically update repo

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-prok-scripts/P12a-remake-barplot-with-PR2-taxonomy.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-prok-scripts/P12a-remake-barplot-with-PR2-taxonomy.sh
@@ -2,10 +2,14 @@
 source ../515FY-926R.cfg
 conda activate $qiime2version
 
+timestamp=`date +"%y%m%d-%H%M"`
+
 qiime taxa barplot \
   --i-table 03-DADA2d/table.qza \
   --i-taxonomy 09-subsetting/tax-merged/chloroplasts-PR2-reclassified-merged-classification.qza \
   --m-metadata-file sample-metadata.tsv \
   --output-dir 07-barplots-PR2-chloroplast-tax
+
+mv 07-barplots-PR2-chloroplast-tax/visualization.qzv 07-barplots-PR2-chloroplast-tax/$timestamp.$studyName.16S.barplot.qzv
 
 conda deactivate
