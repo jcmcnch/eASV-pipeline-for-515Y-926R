575fecfd3abc3d3445232d1df87de84da955db1e
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-09-06 09:47:03 -0700

    automatically update repo

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/setup-scripts/04-setup-new-analysis-directory.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/setup-scripts/04-setup-new-analysis-directory.sh
@@ -6,13 +6,13 @@ if [[ ${#1} -eq 0 ]] ; then
 fi
 
 if [[ ! ${#1} -eq 0 ]] ; then
-    echo "An analysis directory has been created for you at /home/$USER/515FY-926R-pipeline/$1 . It contains all the scripts needed to run the pipeline. Make sure to modify the configuration file to suit your data before running the pipeline."
+	echo "An analysis directory has been created for you at /home/$USER/515FY-926R-pipeline/$1 . It contains all the scripts needed to run the pipeline. The only two things you need to do are: 1) Add your input files into the "00-raw" folder and 2) modify the configuration file to suit these input files. The pipeline assumes your input files are untrimmed fastq.gz files."
 fi
 
 studyName=$1
 
 studyDir=/home/$USER/515FY-926R-pipeline/$studyName
-mkdir -p $studyDir/scripts $studyDir/runscripts $studyDir/01-trimmed $studyDir/02-PROKs/scripts $studyDir/02-EUKs/scripts
+mkdir -p $studyDir/scripts $studyDir/runscripts $studyDir/01-trimmed $studyDir/02-PROKs/scripts $studyDir/02-EUKs/scripts $studyDir/00-raw
 
 cp 00-trimming-sorting-scripts/* 02-utility-scripts/* $studyDir/scripts
 cp 01-prok-scripts/* $studyDir/02-PROKs/scripts
