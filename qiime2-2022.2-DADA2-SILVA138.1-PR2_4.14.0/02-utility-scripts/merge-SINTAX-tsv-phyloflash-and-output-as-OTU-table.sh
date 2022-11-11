135ffcc76c33974f7f653b62a8febab3d3b09e10
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2022-11-10 16:12:33 -0800

    add example script to run SINTAX merging

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/02-utility-scripts/merge-SINTAX-tsv-phyloflash-and-output-as-OTU-table.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/02-utility-scripts/merge-SINTAX-tsv-phyloflash-and-output-as-OTU-table.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -i
-conda activate phyloflash-env
+conda activate phyloseq-env
 
 merge-SINTAX-tsv-phyloflash-and-output-as-OTU-table.R --inputTSV "RAS_WSC_03_2017_2_SSU_rRNA.classified.SILVA138.1.taxtable.tsv,RAS_WSC_05_2017_1_SSU_rRNA.classified.SILVA138.1.taxtable.tsv,RAS_WSC_06_2017_1_SSU_rRNA.classified.SILVA138.1.taxtable.tsv,RAS_WSC_07_2017_1_SSU_rRNA.classified.SILVA138.1.taxtable.tsv,RAS_WSC_07_2017_2_SSU_rRNA.classified.SILVA138.1.taxtable.tsv,RAS_WSC_07_2017_3_SSU_rRNA.classified.SILVA138.1.taxtable.tsv,RAS_WSC_09_2016_1_SSU_rRNA.classified.SILVA138.1.taxtable.tsv,RAS_WSC_10_2016_SSU_rRNA.classified.SILVA138.1.taxtable.tsv,RAS_WSC_11_2016_SSU_rRNA.classified.SILVA138.1.taxtable.tsv,RAS_WSC_12_2016_1_SSU_rRNA.classified.SILVA138.1.taxtable.tsv" --OTUout RAS_WGC_2016-2017.merged.tsv
