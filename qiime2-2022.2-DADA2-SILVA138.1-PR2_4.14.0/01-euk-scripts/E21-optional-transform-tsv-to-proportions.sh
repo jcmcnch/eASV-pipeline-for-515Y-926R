00915925ba9b20e0308da96ccabb00b2cf7c4c13
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2023-03-06 01:03:29 -0800

    Fix script location

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-euk-scripts/E21-optional-transform-tsv-to-proportions.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/01-euk-scripts/E21-optional-transform-tsv-to-proportions.sh
@@ -9,7 +9,7 @@ for item in `ls 20-exports/04-converted-biom-to-tsv/PR2/*biom.tsv`; do
 	outdir=`dirname $item`
 	filestem=`basename $item .tsv`
 
-	transform-ESV-tsv-to-proportions.py --input $item --output $outdir/$filestem.proportions.tsv --minimum_abundance_filtered_output $outdir/$filestem.proportions-minabund-0.01.tsv --minimum_abundance_threshold 0.01 --cumulative_abundance_filtered_output $outdir/$filestem.proportions-cumabund-0.99.tsv --cumulative_abundance_threshold 0.99
+	./scripts/transform-ESV-tsv-to-proportions.py --input $item --output $outdir/$filestem.proportions.tsv --minimum_abundance_filtered_output $outdir/$filestem.proportions-minabund-0.01.tsv --minimum_abundance_threshold 0.01 --cumulative_abundance_filtered_output $outdir/$filestem.proportions-cumabund-0.99.tsv --cumulative_abundance_threshold 0.99
 
 done
 
@@ -20,6 +20,6 @@ for item in `ls 20-exports/04-converted-biom-to-tsv/SILVA/*biom.tsv`; do
         outdir=`dirname $item`
         filestem=`basename $item .tsv`
 
-        transform-ESV-tsv-to-proportions.py --input $item --output $outdir/$filestem.proportions.tsv --minimum_abundance_filtered_output $outdir/$filestem.proportions-minabund-0.01.tsv --minimum_abundance_threshold 0.01 --cumulative_abundance_filtered_output $outdir/$filestem.proportions-cumabund-0.99.tsv --cumulative_abundance_threshold 0.99
+        ./scripts/transform-ESV-tsv-to-proportions.py --input $item --output $outdir/$filestem.proportions.tsv --minimum_abundance_filtered_output $outdir/$filestem.proportions-minabund-0.01.tsv --minimum_abundance_threshold 0.01 --cumulative_abundance_filtered_output $outdir/$filestem.proportions-cumabund-0.99.tsv --cumulative_abundance_threshold 0.99
 
 done
