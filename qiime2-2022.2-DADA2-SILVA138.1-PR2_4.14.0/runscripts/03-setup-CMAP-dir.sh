23265f828f2f89aadb3f85ac20dd71c349f87794
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2024-02-22 17:13:45 -0800

    Make CMAP folder generation script generic

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/runscripts/03-setup-CMAP-dir.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/runscripts/03-setup-CMAP-dir.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -i
 
 source 515FY-926R.cfg
+destination=05-${studyName}_CMAP
 
-mkdir -p 05-${studyName}_CMAP
-cd 05-P16N-S_CMAP
+mkdir -p $destination
+cd $destination
 git clone https://github.com/jcmcnch/OTUandMetadata2CMAP.git
-git clone https://github.com/simonscmap/c-microbial-map.git
+#git clone https://github.com/simonscmap/c-microbial-map.git
 mkdir -p 00-input-subsetted 00-input-tables 00-metadata
 cp ../04-Formatted/*_proportions_reordered.tsv 00-input-tables
-cp ../02-PROKs/210917_sample-metadata-P16N-S_PROK.tsv 00-metadata/sample-metadata.tsv
+#cp ../02-PROKs/210917_sample-metadata-P16N-S_PROK.tsv 00-metadata/sample-metadata.tsv
 #take top 1000 ASVs
-for item in 00-input-tables/*tsv ; do head -n1001 $item > 00-input-subsetted/`basename $item .tsv`.head1001.tsv ; done
+#for item in 00-input-tables/*tsv ; do head -n1001 $item > 00-input-subsetted/`basename $item .tsv`.head1001.tsv ; done
