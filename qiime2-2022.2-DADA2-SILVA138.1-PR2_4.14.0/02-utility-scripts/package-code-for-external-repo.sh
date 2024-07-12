d4f88224be5fa1f787bceb6476dcea94479ca04e
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2024-07-12 08:33:42 -0700

    Update packaging script (code) for easier use

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/02-utility-scripts/package-code-for-external-repo.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/02-utility-scripts/package-code-for-external-repo.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -i
-mkdir -p  code-export/02-EUKs/ code-export/02-PROKs
-cp -r *cfg runscripts scripts code-export/
+
+source 515FY-926R.cfg
+timestamp=`date +"%y%m%d-%H%M"`
+outDir=$HOME/515-926R-code-exports/${timestamp}_${studyName}_code-export
+
+mkdir -p  $outDir/02-EUKs/ $outDir/02-PROKs
+cp -r *cfg runscripts scripts $outDir/
 
 #scripts
-cp -r 02-PROKs/*tsv 02-PROKs/*txt 02-PROKs/scripts/ code-export/02-PROKs
-cp -r 02-EUKs/*tsv 02-EUKs/*txt 02-EUKs/scripts/ code-export/02-EUKs
+cp -r 02-PROKs/*tsv 02-PROKs/*txt 02-PROKs/scripts/ $outDir/02-PROKs
+cp -r 02-EUKs/*tsv 02-EUKs/*txt 02-EUKs/scripts/ $outDir/02-EUKs
 
-echo "This directory contains scripts from a custom pipeline for analyzing amplicons from the 515Y/926R primers (https://github.com/jcmcnch/eASV-pipeline-for-515Y-926R). The configuration file included here specifies parameters that the user chose during the analysis." > code-export/README.md
+echo "This directory contains scripts from a custom pipeline for analyzing amplicons from the 515Y/926R primers (https://github.com/jcmcnch/eASV-pipeline-for-515Y-926R). The configuration file included here specifies parameters that the user chose during the analysis." > $outDir/README.md
