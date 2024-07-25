927ae39409444c21aae71457ef16e88b28139eea
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2024-07-25 07:05:44 -0700

    Added loop for capturing scripts from multiple sequencing runs

--- qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/02-utility-scripts/package-code-for-external-repo.sh
+++ qiime2-2022.2-DADA2-SILVA138.1-PR2_4.14.0/02-utility-scripts/package-code-for-external-repo.sh
@@ -3,12 +3,27 @@
 source 515FY-926R.cfg
 timestamp=`date +"%y%m%d-%H%M"`
 outDir=$HOME/515Y-926R-code-exports/${timestamp}_${studyName}_code-export
+mkdir $outDir
 
-mkdir -p  $outDir/02-EUKs/ $outDir/02-PROKs
+#copy basic info and overall runscripts
 cp -r *cfg runscripts scripts $outDir/
 
-#scripts
-cp -r 02-PROKs/*tsv 02-PROKs/*txt 02-PROKs/scripts/ $outDir/02-PROKs
-cp -r 02-EUKs/*tsv 02-EUKs/*txt 02-EUKs/scripts/ $outDir/02-EUKs
+#loops added to capture scripts for analyses with multiple sequencing runs where there will be more than one "02-PROKs" or "02-EUKs" directories per runscript that handles multiple sequencing runs
+
+#loop through multiple analysis directories, if present
+for item in 02-PROKs* ; do
+
+	mkdir -p $outDir/$item
+	cp -r $item/*tsv $item/*txt $item/scripts/ $outDir/$item 2> /dev/null
+
+done
+
+#loop through multiple analysis directories, if present
+for item in 02-EUKs* ; do
+
+        mkdir -p $outDir/$item
+	cp -r $item/*tsv $item/*txt $item/scripts/ $outDir/$item 2> /dev/null
+
+done
 
 echo "This directory contains scripts from a custom pipeline for analyzing amplicons from the 515Y/926R primers (https://github.com/jcmcnch/eASV-pipeline-for-515Y-926R). The configuration file included here specifies parameters that the user chose during the analysis." > $outDir/README.md
