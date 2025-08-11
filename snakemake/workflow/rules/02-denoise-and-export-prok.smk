d18b6f1454c89240c9fa4c15c5d8640288f5ac15
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2025-08-11 11:18:38 -0300

    Update qiime2 workflow

---
+++ snakemake/workflow/rules/02-denoise-and-export-prok.smk
@@ -0,0 +1,30 @@
+rule create_manifest_prok:
+    input:
+        r1=temp("results/01-split/{sample}.prok.R1.fastq.gz"),
+        r2=temp("results/01-split/{sample}.prok.R2.fastq.gz")
+    output:
+        manifest.tsv
+
+rule import_prok:
+    input:
+        "results/01-split/{sample}.prok.fastq"
+    output:
+        out="results/01-split/{sample}.prok.R1.fastq.gz",
+        out2="results/01-split/{sample}.prok.R2.fastq.gz"
+    conda:
+        "../envs/qiime2-amplicon-ubuntu-2025-7-conda.yml"
+    shell:
+        "bbsplit.sh build=1 ref={input.file1},{input.file2} path={output}"
+
+    log:
+        "logs/x.log"
+
+    params:
+        command="reformat.sh",
+        overwrite=True,  # recommended
+        pigz=True,
+    threads: 4
+    resources:
+        mem_mb=4000,  # optional: bbmap normaly needs a lot of memory, e.g. 60GB
+    wrapper:
+        "v7.2.0/bio/bbtools"
---
+++ snakemake/workflow/scripts/P00-create-manifest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -i
+
+#shortcut for making manifests based on fasta file names
+#script assumes you have the same number of FWD and REV reads and that they're named in a meaninful way (i.e. samplename.1.fastq.gz)
+
+#remove empty files
+find ./00-fastq -size  0 -print0 | xargs -0 rm -- 2> /dev/null
+
+#the bit you want to cut from the file names, leaving only the sample name
+cutME=".trimmed.SILVA_132_PROK.cdhit95pc_1.fastq" 
+
+printf "sample-id	forward-absolute-filepath	reverse-absolute-filepath\n" > manifest.tsv
+
+for item in `ls 00-fastq/*1.fastq` ; do 
+	
+	sampleID=`basename $item $cutME | sed 's/_/-/g'` #remove underscores before importing
+	sampleIDstring=`basename $item $cutME`
+	R1=`ls 00-fastq/$sampleIDstring.trimmed.SILVA_132_PROK.cdhit95pc_1.fastq`
+	R2=`ls 00-fastq/$sampleIDstring.trimmed.SILVA_132_PROK.cdhit95pc_2.fastq`
+
+	printf "$sampleID	\$PWD/$R1	\$PWD/$R2\n" >> manifest.tsv
+
+done
