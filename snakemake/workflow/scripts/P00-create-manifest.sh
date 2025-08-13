f1f0b10f5e0c11fc527cc493c42d6cf735f237dd
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2025-08-13 10:58:27 -0300

    update bash testing script

--- snakemake/workflow/scripts/P00-create-manifest.sh
+++ snakemake/workflow/scripts/P00-create-manifest.sh
@@ -4,20 +4,16 @@
 #modified for snakemake
 
 #remove empty files
-find ./results/01-split/*prok* -size  0 -print0 | xargs -0 rm -- 2> /dev/null
+#find ./results/01-split/*prok* -size 0 -print0 | xargs -0 rm -- 2> /dev/null
 
 #the bit you want to cut from the file names, leaving only the sample name
 cutME=".prok.R1.fastq.gz" 
 
 printf "sample-id	forward-absolute-filepath	reverse-absolute-filepath\n" > ${snakemake_output[0]}
-
-for item in `ls results/01-split/*.prok.R1.fastq.gz` ; do 
 	
-	sampleID=`basename $item $cutME | sed 's/_/-/g'` #remove underscores before importing
-	sampleIDstring=`basename $item $cutME`
-	R1=`ls 00-fastq/$sampleIDstring.prok.R1.fastq.gz`
-	R2=`ls 00-fastq/$sampleIDstring.prok.R2.fastq.gz`
+sampleID=`echo ${snakemake_wildcards[sample]} $cutME | sed 's/_/-/g'` #remove underscores before importing
+R1=${snakemake_input[0]}
+R2=${snakemake_input[1]}
 
-	printf "$sampleID	\$PWD/$R1	\$PWD/$R2\n" >> ${snakemake_output[0]}
+printf "$sampleID	\$PWD/$R1	\$PWD/$R2\n" >> ${snakemake_output[0]}
 
-done
