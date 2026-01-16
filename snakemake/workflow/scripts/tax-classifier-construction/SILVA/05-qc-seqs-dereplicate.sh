3ed9dad2c67c7613e66e19258db3a5cb0f35ec70
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2026-01-16 18:19:03 -0400

    Remove silva rank handle option, as does not appear to work anymore

--- snakemake/workflow/scripts/tax-classifier-construction/SILVA/05-qc-seqs-dereplicate.sh
+++ snakemake/workflow/scripts/tax-classifier-construction/SILVA/05-qc-seqs-dereplicate.sh
@@ -3,7 +3,7 @@
 qiime rescript dereplicate \
     --i-sequences ${snakemake_input[filteredDNA]} \
     --i-taxa ${snakemake_input[taxonomy]} \
-    --p-rank-handles 'silva' \
     --p-mode 'uniq' \
     --o-dereplicated-sequences ${snakemake_output[dereplicatedDNA]} \
     --o-dereplicated-taxa ${snakemake_output[dereplicatedTaxa]}
+#   --p-rank-handles 'silva' #removed because 'silva' option apparently no longer works
--- snakemake/workflow/scripts/tax-classifier-construction/SILVA/07-deduplicate-sliced-data.sh
+++ snakemake/workflow/scripts/tax-classifier-construction/SILVA/07-deduplicate-sliced-data.sh
@@ -3,8 +3,7 @@
 qiime rescript dereplicate \
     --i-sequences {snakemake_input[slicedDNA]} \
     --i-taxa silva-138.1-ssu-nr99-tax-derep-uniq.qza \
-    --p-rank-handles 'silva' \
     --p-mode 'uniq' \
     --o-dereplicated-sequences {snakemake_output[slicedDNAdereplicated]} \
     --o-dereplicated-taxa {snakemake_output[dereplicatedTaxaSliced]} 
-
+#   --p-rank-handles 'silva' #removed because 'silva' option apparently no longer works
