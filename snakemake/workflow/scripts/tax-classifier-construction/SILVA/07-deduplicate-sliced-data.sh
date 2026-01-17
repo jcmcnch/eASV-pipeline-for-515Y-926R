fe0234ca8726019a6862a805b58b54fb3a690976
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2026-01-16 23:12:13 -0400

    Fix more typos

--- snakemake/workflow/scripts/tax-classifier-construction/SILVA/07-deduplicate-sliced-data.sh
+++ snakemake/workflow/scripts/tax-classifier-construction/SILVA/07-deduplicate-sliced-data.sh
@@ -2,7 +2,7 @@
 
 qiime rescript dereplicate \
     --i-sequences ${snakemake_input[slicedDNA]} \
-    --i-taxa silva-138.1-ssu-nr99-tax-derep-uniq.qza \
+    --i-taxa ${snakemake_input[dereplicatedTaxa]} \
     --p-mode 'uniq' \
     --o-dereplicated-sequences ${snakemake_output[slicedDNAdereplicated]} \
     --o-dereplicated-taxa ${snakemake_output[dereplicatedTaxaSliced]} 
