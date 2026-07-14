5cdd944272661fc25160b867c6f91fb6d22dd928
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2026-07-14 14:47:40 -0300

    add headers to output

--- snakemake/workflow/scripts/concat-eukfrac.sh
+++ snakemake/workflow/scripts/concat-eukfrac.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+if [ -s ${snakemake_output[eukfracall]} ] ; then
+
+#if not empty, fill lines
 cat ${snakemake_input[eukfrac]} >> ${snakemake_output[eukfracall]}
+
+else
+
+#if file empty, add header
+printf "sample\tprok_seqs_split\teuk_seqs_split\ttotal_seqs_split\teukfrac_split\n" >>  ${snakemake_output[eukfracall]}
+cat ${snakemake_input[eukfrac]} >> ${snakemake_output[eukfracall]}
+
+fi
