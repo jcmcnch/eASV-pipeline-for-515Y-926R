cb9f43d1e24fddb995f648229e0c8a683b78aff9
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2026-07-15 15:45:56 -0300

    Update naming scheme for intstds in bash script

--- snakemake/workflow/scripts/B01-blast-ASVs.sh
+++ snakemake/workflow/scripts/B01-blast-ASVs.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 #require 100% coverage, 99% ID which gives 1-2 mismatches for our amplicon region (515Y/926R), probably will work for most cases
-db=config/intstd_fastas/`basename ${snakemake_input[BP]} .nhr`
-blastn -query ${snakemake_input[latestseqs]} -db $db -outfmt 6 -perc_identity 99 -qcov_hsp_perc 100 > ${snakemake_output[BPtsv]}
-cut -f1 ${snakemake_output[BPtsv]} > ${snakemake_output[BPasvs]}
+db=config/intstd_fastas/`basename ${snakemake_input[intstd1]} .nhr`
+echo blastn -query ${snakemake_input[latestseqs]} -db $db -outfmt 6 -perc_identity 99 -qcov_hsp_perc 100 > ${snakemake_output[intstd1tsv]}
+blastn -query ${snakemake_input[latestseqs]} -db $db -outfmt 6 -perc_identity 99 -qcov_hsp_perc 100 > ${snakemake_output[intstd1tsv]}
+cut -f1 ${snakemake_output[intstd1tsv]} > ${snakemake_output[intstd1asvs]}
 
-db=config/intstd_fastas/`basename ${snakemake_input[DR]} .nhr`
-blastn -query ${snakemake_input[latestseqs]} -db $db -outfmt 6 -perc_identity 99 -qcov_hsp_perc 100 > ${snakemake_output[DRtsv]}
-cut -f1 ${snakemake_output[DRtsv]} > ${snakemake_output[DRasvs]}
+db=config/intstd_fastas/`basename ${snakemake_input[intstd2]} .nhr`
+blastn -query ${snakemake_input[latestseqs]} -db $db -outfmt 6 -perc_identity 99 -qcov_hsp_perc 100 > ${snakemake_output[intstd2tsv]}
+cut -f1 ${snakemake_output[intstd2tsv]} > ${snakemake_output[intstd2asvs]}
 
-db=config/intstd_fastas/`basename ${snakemake_input[TT]} .nhr`
-blastn -query ${snakemake_input[latestseqs]} -db $db -outfmt 6 -perc_identity 99 -qcov_hsp_perc 100 > ${snakemake_output[TTtsv]}
-cut -f1 ${snakemake_output[TTtsv]} > ${snakemake_output[TTasvs]}
+db=config/intstd_fastas/`basename ${snakemake_input[intstd3]} .nhr`
+blastn -query ${snakemake_input[latestseqs]} -db $db -outfmt 6 -perc_identity 99 -qcov_hsp_perc 100 > ${snakemake_output[intstd3tsv]}
+cut -f1 ${snakemake_output[intstd3tsv]} > ${snakemake_output[intstd3asvs]}
